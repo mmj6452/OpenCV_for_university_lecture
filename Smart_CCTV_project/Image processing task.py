76273cda4676397b4e07f5131a47fddc6a83c0e0
mmj6452 <645277@naver.com>
2024-06-04 16:26:57 +0900

    Smart_CCTV_upload

---
+++ Smart_CCTV_project/Image processing task.py
@@ -0,0 +1,167 @@
+
+import tensorflow
+import numpy as np
+import cv2
+#from function.AI_CCTV import AI_processing
+from function.AI_CCTV import draw_warning
+from function.AI_CCTV import Layout_resize
+from function.AI_CCTV import image_sum
+#from function.AI_CCTV import make_layout
+from function.AI_CCTV import histogram_equalization
+
+# 모델 주소 저장
+#model_address ='Model/keras_model.h5'
+# 모델값 가져오기
+#model = tensorflow.keras.models.load_model(model_address)
+
+#opencv 자체 사람감지 모델
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
+
+# 카메라에서 비디오 읽어오기
+webcam = cv2.VideoCapture(0)
+if webcam.isOpened() == False:
+    raise Exception("카메라 연결이 안됩니다.")
+
+# 읽어올 프레임을 qHD로 맞춰주기
+webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+webcam.set(cv2.CAP_PROP_ZOOM,1)
+webcam.set(cv2.CAP_PROP_FOCUS,0)
+
+#비디오 writer 구성
+size = (round(webcam.get(cv2.CAP_PROP_FRAME_WIDTH)),round(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+fps = 4
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+out = cv2.VideoWriter('video/output.avi', fourcc, fps, size)
+if out.isOpened() == False: 
+    raise Exception("비디오 쓰기 불가")
+    webcam.release()
+    sys.exit()
+    
+#Latout 이미지 가져오기
+main_clicked = Layout_resize(cv2.imread("Layout/main_clicked.png"))
+main_unclicked = Layout_resize(cv2.imread("Layout/main_unclicked.png"))
+edge_clicked = Layout_resize(cv2.imread("Layout/edge_clicked.png"))
+edge_unclicked = Layout_resize(cv2.imread("Layout/edge_unclicked.png"))
+improved_clicked = Layout_resize(cv2.imread("Layout/improved_clicked.png"))
+improved_unclicked = Layout_resize(cv2.imread("Layout/improved_unclicked.png"))
+original_clicked = Layout_resize(cv2.imread("Layout/original_clicked.png"))
+original_unclicked = Layout_resize(cv2.imread("Layout/original_unclicked.png"))
+
+#변수 선언 및 초기화
+human_detect = False
+frame_cnt = 0
+frame_num = 0
+Layout_state = 0
+#bool 값들을 더해서 두개이상의 조건이 감지되면 빠르게 반응
+detection_weight = human_detect
+
+#무한반복
+while True:
+    #카메라에서 프레임가져오기 ret은 bool값으로 프레임이 가져와지면 True
+    ret, frame = webcam.read()
+    if not ret: break
+    save = frame
+    frame_num = frame_num + 1
+    frame = cv2.resize(frame, (1280,960), interpolation=cv2.INTER_AREA)
+
+    print("사람이 감지된 프레임갯수",frame_cnt)
+    detection_weight = human_detect
+
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
+
+    print(weights)
+
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                      (0, 255, 0), 2)
+
+
+    
+    #키입력을 받는다
+    #d를 입력하면 break 오른쪽 화살표:Layout_state를 +1해준다 왼쪽은 반대
+    Key = cv2.waitKeyEx(100)
+    if Key == 100: 
+        break
+    if Key == 2555904:
+        Layout_state = Layout_state + 1
+        if Layout_state > 3:
+            Layout_state = 0
+           
+    if Key == 2424832:
+        Layout_state = Layout_state - 1
+        if Layout_state < 0:
+            Layout_state = 3
+            
+    
+    #읽어온 프레임을 AI처리해서 0 ~ 1의 사이의 사람일 확률과 아닐 확률이 나온다.
+    #Image_prediction = AI_processing(frame,model)
+
+    #레이아웃 만드는 부분을 함수로 제작했지만 작동이 안되는 문제가 생겨서 바꿈
+    if (Layout_state == 0):
+        S_img_1 = histogram_equalization(frame)
+        S_img_2 = cv2.Canny(frame,100,255)
+        frame = image_sum(frame,S_img_1,S_img_2)
+        frame[20:84,752:864] =  main_clicked
+        frame[20:84,884:996] =  original_unclicked
+        frame[20:84,1016:1128] =  improved_unclicked
+        frame[20:84,1148:1260] =  edge_unclicked
+        
+    elif (Layout_state == 1):
+        frame[20:84,752:864] =  main_unclicked
+        frame[20:84,884:996] =  original_clicked
+        frame[20:84,1016:1128] =  improved_unclicked
+        frame[20:84,1148:1260] =  edge_unclicked
+        
+    elif (Layout_state == 2):
+        frame = histogram_equalization(frame)
+        #sharpening(frame)
+        frame[20:84,752:864] =  main_unclicked
+        frame[20:84,884:996] =  original_unclicked
+        frame[20:84,1016:1128] =  improved_clicked 
+        frame[20:84,1148:1260] =  edge_unclicked
+    elif (Layout_state == 3):
+        #Canny기법으로 엣지를 찾아낸다
+        frame = cv2.Canny(frame,100,255)
+        #그레이스케일 이미지를 BGR형태로 반환한다.
+        frame = cv2.cvtColor(frame,cv2.COLOR_GRAY2BGR)
+        frame[20:84,752:864] =  main_unclicked
+        frame[20:84,884:996] =  original_unclicked
+        frame[20:84,1016:1128] =  improved_unclicked
+        frame[20:84,1148:1260] =  edge_clicked
+    
+    
+    #이미지에 사람이 없을 확률이 더 높은므로 감지 변수를 초기화해준다
+    if weights is ():
+        print('Non')
+        cv2.putText(frame, 'non', (0, 25), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        frame_cnt = 0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+        human_detect = False
+
+    #이미지에 사람이 있을 확률이 더 높으므로 프레임카운트에 +1을 해주고 human_detect를 true로 만들어준다.
+    else:
+        cv2.putText(frame, 'Detect', (0, 25), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        print('Detect')
+        human_detect = True
+        frame_cnt = frame_cnt + detection_weight
+        
+        path = 'CCTV_img/'+str(frame_num)+'.jpg'
+        print("해당 프레임이 저장되었습니다."+str(path))
+        cv2.imwrite(str(path),save)
+        out.write(save)
+        
+    #10개의 프레임이 지날때까지 사람이 계속 있을 경우    
+    if (frame_cnt >= 10):
+        #짝수 프레임일때 붉은색으로 경고를 해준다.
+        if(frame_cnt % 2 == 0):
+            draw_warning(frame)
+            
+    cv2.imshow("VideoFrame", frame)
+out.release()
+webcam.release()
---
+++ Smart_CCTV_project/README.md
@@ -0,0 +1,94 @@
+# Smart_CCTV
+ 
+IoT영상통신시스템의 기말고사 대체 프로젝트
+
+사전설치해야할 라이브러리
+
+<pre><code>
+    pip install opencv-python
+    pip install tensorflow
+    </code></pre>
+
+
+
+폴더 구조
+CCTV_img폴터는 사람이 감지되었을때 프레임들을 전부 이미지로써 저장하는 파일이다
+video폴더는 사람이 감지되었을때 프레임들을 영상으로 저장하는 파일이다.
+function폴더는 해당 프로그램을 구현함으로써 필요한 함수들을 구현하여 AI_CCTV라는 파일로 저장해두었다
+Layout폴더는 Layout 이미지를 모아서 png형태로 저장해두고 docx파일로도 모아두었다.
+Model폴더에는 Roboflow에서 받아온 사람이미지 데이터셋을 이용하여 티쳐블머신에 돌려만든 카라스 모델이 저장되어있다.
+
+
+image processing task.py는 구현한 함수를 통해 실질적으로 작동하는 코드이다.
+모델읽어오기
+카메라에서 비디오 읽어오기
+프레임을 비디오 또는 이미지로 저장하기
+프레임을 읽어와서 사람이 감지되었는지 확인하기
+감지가 되면 경고해주기
+영상을 엣지,히스토그램평활화등의 방법으로 전처리하기
+레이아웃 구성하기
+등의 기능들이 구현되어있다.
+
+AI_CCTV.py는 위의 image processing task를 구현하기 위해 필요한 함수들을 모아놓은 파일이다.
+
+
+모델을 이용하여 사람이 감지되었는지 확인하는 함수
+<pre><code>AI_processing(frame,model)</code></pre>
+frame에는 이미지를 넣어주며
+model에는 keras모델파일의 경로를 지정해준다
+해당하는 오브젝트가 존재할 확률를 배열로 반환해준다.
+
+해당 모델의 이미지 데이터의 부족으로 정확성 문제가 발생
+opencv에서 기본으로 제공해주는 Yolo3를 활용한 모델을 가져와 정확도 개선과 사람의 위치까지 확인이 가능하다 하지만 연산속도에서 더욱더 느려지는 단점이 존재함
+연산속도가 중요할경우 기존의 함수로 다시 바뀌줄것
+<br/><br/>
+
+프레임에 경고해주는 붉은 사각형을 그리는 함수
+<pre><code>draw_warning(frame)</code></pre>
+frame에 이미지를 넣어준다.
+해당이미지의 테두리에 붉은색 테두리를 그려서 이미지를 반환해준다.
+<br/><br/>
+레이아웃 사이즈를 조절하는 함수
+<pre><code>Layout_resize(img)</code></pre>
+img에 이미지를 넣어주면
+해당이미지를 112*64의 크기로 반환해준다.
+<br/><br/>
+필터로 컨볼루션을 진행해주는 함수
+<pre><code>
+    filter(image , mask)
+    filter2(image , mask)
+</code></pre>
+image에 이미지를 넣어준다
+mask에 mask를 넣어준다.
+이미지에 마스크를 이용하여 컨볼루션을 한후 이미지값을 반환한다.
+<br/><br/>
+레이아웃을 만들어주는 함수
+<pre><code>make_layout(frame,Layout_state,original_clicked,original_unclicked,edge_clicked,edge_unclicked,improved_clicked,improved_unclicked)</code></pre>
+프레임에 이미지
+그후 112*64크기에 각각맞는 버튼에 맞는 이미지를 넣어주면된다.
+frame 오른쪽위에 버튼 3개를 생성해준다.
+해당 함수가 함수로 작성해주면 작동안되는 문제가 발생하여 실제코드에는 사용하지 않고 직접 넣어줬다
+<br/><br/>
+밝기값에 대해 히스토그램이퀄라이제이션을 진행해주는 함수
+<pre><code>histogram_equalization(frame)</code></pre>
+frame에 이미지를 넣어준다
+밝기값에 대해서만 이퀄라이제이션을 진행한 이미지를 반환해준다.
+<br/><br/>
+이미지 샤프닝을 해주는 함수
+<pre><code>sharpening(frame)</code></pre>
+image에 이미지를 넣어준다
+이미지에 대해 샤프닝을 진행한 이미지를 반환해준다.
+<br/><br/>
+3개의 이미지를 한화면에 보여주기 위해 합쳐주는 함수
+<pre><code>image_sum(B_img,S_img_1,S_img_2)</code></pre>
+B_img 좌측에 띄어주고 싶은 이미지를 넣어준다 (이미지의 크기문제로 절반이 잘린형태로 반환이 된다.)
+S_img_1 오른쪽 위쪽에 띄어주고 싶은 이미지를 넣어준다.
+S_img_2 왼쪽 위쪽에 띄어주고 싶은 이미지를 넣어준다.
+3개의 이미지를 합쳐서 하나의 화면에 나타내 준다.
+
+
+/////////////////////////////////////////////////////////
+
+버그났을때 대처법 경우 
+
+경로에 이상이 있을 경우 처음 프로젝트를 열때 파이참에서 파이참 프로젝트 열기를 smart CCTV폴더로 지정해줄것
